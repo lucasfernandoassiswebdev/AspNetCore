77108a30961011eed668588993068e55acf7a6bf
AzureAD\LUCASFERNANDODEASSIS <lucasaassis@outlook.com>
2018-04-08 21:16:37 -0300

    master - Adicionando post put e delete na web api

--- ApiUsuarios/Controllers/UsuariosController.cs
+++ ApiUsuarios/Controllers/UsuariosController.cs
@@ -28,5 +28,44 @@ namespace ApiUsuarios.Controllers
             
             return new ObjectResult(usuario);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Usuario usuario){
+            if(usuario == null)
+                return BadRequest();
+
+            _repositorio.Add(usuario);
+
+            return CreatedAtRoute("GetUsuario", new Usuario{UsuarioId = usuario.UsuarioId}, usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Usuario usuario){
+            if(usuario == null || usuario.UsuarioId != id)
+                return BadRequest();
+            
+            var _usuario = _repositorio.Find(id);
+            if(usuario == null)
+                return NotFound();
+            
+            _usuario.Email = usuario.Email;
+            _usuario.Nome = usuario.Nome;
+
+            _repositorio.Update(_usuario);
+
+            //Retorna um 204(No Content)
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id){
+            var usuario = _repositorio.Find(id);
+            if(usuario == null)
+                return NotFound();
+
+            _repositorio.Remove(id);
+
+            return new NoContentResult();
+        }
     }
 }
